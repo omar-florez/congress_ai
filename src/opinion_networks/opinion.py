5557d667536f5dd92a9c8327015865cd585f1e55
Omar Florez <oflorez@omars-mbp.lan>
2023-08-04 00:19:12 -0700

    Reset

--- src/opinion_networks/opinion.py
+++ src/opinion_networks/opinion.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from opinion_networks.engine import Value
 import json
+import pdb
 
 class Opinion:
     def __init__(self, opinion_type: str, reasoning: str, score: Value):
@@ -22,6 +23,7 @@ class OpinionPair:
     def __init__(self, opinions_json: str, law_str: str):
         self.law: str = law_str
         try:
+            pdb.set_trace()
             opinions = self.from_json(opinions_json)
         except:
             pdb.set_trace()
