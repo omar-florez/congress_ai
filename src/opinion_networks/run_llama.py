e1c7457d8da3d9ab43be172a3179de7aa226af10
Omar Florez <oflorez@omars-mbp.lan>
2023-08-14 00:55:11 -0700

    add run code for llama

--- src/opinion_networks/run_llama.py
+++ src/opinion_networks/run_llama.py
@@ -74,18 +74,10 @@ def load_llm(model_id, hf_auth):
     return (model, llm)
 
 def run(model_id):
-    #model_id = 'meta-llama/Llama-2-70b-chat-hf'
-    #model_id = "meta-llama/Llama-2-7b-hf"
-    model_id = "meta-llama/Llama-2-7b-chat-hf"
     # Info: https://huggingface.co/docs/hub/security-tokens
-    hf_auth = 'INSERT YOUR HUGGINGFACE AUTHORIZATION TOKEN TO DOWNLOAD HF MODELS'
+    hf_auth = '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'
     model, llm = load_llm(model_id, hf_auth)
     pdb.set_trace()
-
-    
-if __name__ == '__main__':
-    
-
 
     os.environ["SERPAPI_API_KEY"] = ""
     os.environ["OPENAI_API_KEY"] = ""
@@ -138,3 +130,10 @@ if __name__ == '__main__':
         print(f'epoch {epoch},  iteration: {i}, loss: {loss}')
         pdb.set_trace()
         trace_graph.draw_dot(loss, format='png', output_filepath=f'./data/peru/laws/summaries/epoch_{epoch}')
+
+    
+if __name__ == '__main__':
+    #model_id = 'meta-llama/Llama-2-70b-chat-hf'
+    #model_id = "meta-llama/Llama-2-7b-hf"
+    model_id = "meta-llama/Llama-2-7b-chat-hf"
+    run(model_id)
--- src/setup_colab.sh
+++ src/setup_colab.sh
@@ -2,3 +2,6 @@ git clone https://github.com/omar-florez/congress_ai.git
 pip install -qU transformers accelerate einops langchain xformers bitsandbytes
 pip install -r congress_ai/requirements.txt
 pip install openai
+
+#cd congress_ai/src/
+#python opinion_networks/run_llama.py
